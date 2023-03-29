4bc5b9f2c56bbf072032eba5897cba5f406e31f0
Administrator <Administrator@DESKTOP-9HU6DH7>
2023-03-30 03:05:44 +0530

    star pattern package added

---
+++ learning_python/star_pattern/Pattern_Template.py
@@ -0,0 +1,12 @@
+'''
+Created on Mar 30, 2023
+
+@author: Administrator
+'''
+
+print("Patter Template")
+
+for i in range(1, 6):
+    for j in range(1, 6):
+        print("*", end = " ")
+    print()
